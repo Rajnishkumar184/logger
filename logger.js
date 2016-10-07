68914cc9232f59c4da8b445dde7325465106a114
Rajnish Kumar <rajnishkumar@Rajnishs-MacBook-Air.local>
2016-10-07 16:58:20 +0530

    implemented file name, function name,line number and console functionality

--- logger.js
+++ logger.js
@@ -5,7 +5,8 @@
 
 var fs=require('fs');
 var Promise=require('bluebird');
-var path=require('path');
+var path = require('path');
+var colors = require('colors');
 var appDir = path.dirname(require.main.filename);
 
 Date.prototype.yyyymmdd = function() {
@@ -13,16 +14,42 @@ Date.prototype.yyyymmdd = function() {
     var dd = this.getDate();
     return [this.getFullYear(), mm, dd].join('-');
 };
-
+colors.setTheme({
+    info: 'green',
+    warn: 'yellow',
+    debug: 'blue',
+    error: 'red'
+});
 var logger={
-    createLog:function (directory,type,message) {
+    log:function (message,type,directory,file) {
 
         var date = new Date();
-        var filepath='/'+directory + '/'+date.yyyymmdd()+'.txt';
+
+        if(!type){
+            type='info';
+        }
+        if(!directory){
+            directory='log';
+        }
+        if(!file){
+            file=date.yyyymmdd();
+        }
+        var filepath='/'+directory + '/'+file+'.log';
+
+        var logLineDetails = ((new Error().stack).split("at ")[2].trim());
+        logLineDetails=logLineDetails.split(' ');
+        var function_name='';
+        if(logLineDetails[0]!='Object.<anonymous>'){
+            function_name=logLineDetails[0];
+        }
+        var file_data=logLineDetails[1].substring(logLineDetails[1].lastIndexOf('/')+1,logLineDetails[1].length-1).split(':');
+        var file_name=file_data[0];
+        var line_number=file_data[1]+':'+file_data[2];
 
         this.dirExist(directory).then(function (data) {
             fs.exists(filepath, function(exists){
-                fs.appendFile(appDir +filepath,type+' : '+message+ ', Time : '+Date.now()+';\n', { flags: 'a+' },function(err) {
+                message='Time : '+Date.now()+' | '+type+' => '+message+", "+file_name+" "+function_name+" "+line_number;
+                fs.appendFile(appDir +filepath,message+'\n', { flags: 'a+' },function(err) {
                     if(err) {
                         //console.log(err);
                     }
@@ -51,9 +78,40 @@ var logger={
                 }
             });
         });
+    },
+    console:function(message,type){
+        if(!type){
+            type='info';
+        }
+        var logLineDetails = ((new Error().stack).split("at ")[2].trim());
+        logLineDetails=logLineDetails.split(' ');
+        var function_name='';
+        if(logLineDetails[0]!='Object.<anonymous>'){
+            function_name=logLineDetails[0];
+        }
+        var file_data=logLineDetails[1].substring(logLineDetails[1].lastIndexOf('/')+1,logLineDetails[1].length-1).split(':');
+        var file_name=file_data[0];
+        var line_number=file_data[1]+':'+file_data[2];
+        message='Time : '+Date.now()+' | '+type+' => '+message+", "+file_name+" "+function_name+" "+line_number;
+        switch(type){
+            case 'debug':
+                console.log(message.debug);
+                break;
+            case 'warn':
+                console.log(message.warn);
+                break;
+            case 'info':
+                console.log(message.info);
+                break;
+            case 'error':
+                console.log(message.error);
+                break;
+            default:
+                console.log(message.info);
+                break;
+        }
     }
 };
 
 module.exports=logger;
-
 
